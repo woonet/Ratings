c357492e739802abdff93de37cf2949ebbadc692
禹相植 <woo9347@gmail.com>
2014-12-06 15:51:00 +0900

    Initial Commit

---
+++ Ratings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ratings
+//
+//  Created by 禹相植 on 2014/12/06.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
